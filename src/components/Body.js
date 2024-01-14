ebf05a7b7bf88525f56c52c6ad8442dc1cd74850
rachitsgh <rachit2351@gmail.com>
2024-01-14 21:26:33 +0530

    Search bar added

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,6 +9,8 @@ const Body=()=>{
 
     //State variable - super powerfull variable
     const [listrestraun,setlistrestraun/* this is funcn*/ ]=useState([]);
+
+    const [searchtext,setsearchtext]=useState("");
 /* 
     const arr = useState(reslist)
       
@@ -93,9 +95,21 @@ if(listrestraun.length == 0){
     //                 "totalRatingsString": "1K+",
     //                 },
     //             }];
-    return listrestraun.length==0?<Shimmer/> : (<div className="Body">
-        <div className="search">search</div>
-        <button className="filter-btn btncss" onClick={()=>{
+    return listrestraun.length==0?<Shimmer/> : (
+    <div className="Body">
+        <div className="filter">
+            <div className="search">
+            <div className="inputBox_container ">
+                <svg className="search_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" alt="search icon">
+                <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
+                </svg>
+            <input className="inputBox search-box" id="inputBox" type="text" placeholder="Search For Restaurant" value={searchtext} onClick={()=>{
+                
+            }}/>
+            </div>
+            <button className="Search-btn btncss">Search</button>
+            </div>
+            <button className="filter-btn btncss" onClick={()=>{
             //filter logic here
             console.log("fg")
             const filteredlistrestraun=listrestraun.filter((res)=>res?.info?.avgRating>=4);
@@ -103,6 +117,7 @@ if(listrestraun.length == 0){
 
 
         }}>Top Rated Restraun..</button>
+        </div>
         <div className="restcontainer" /*style={style} */ >
             {/* <Restaurant resdata={resobj[0]}/>
             <Restaurant resdata={resobj[1]}/> */
