03e57da098b743386015f6f145cb9db448ba7cca
rachitsgh <rachit2351@gmail.com>
2024-01-16 01:00:18 +0530

    fixed search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import Restaurant from "./Restaurant";
-import reslist from "../../utils/Mockdata";
+// import reslist from "../../utils/Mockdata";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 
@@ -9,6 +9,7 @@ const Body=()=>{
 
     //State variable - super powerfull variable
     const [listrestraun,setlistrestraun/* this is funcn*/ ]=useState([]);
+    const [filteredrestraun,setfilteredrestraun]=useState([]);
 
     const [searchtext,setsearchtext]=useState("");
 /* 
@@ -29,12 +30,15 @@ useEffect(()=>{
    fetchdata();
 },[])
 
+// whenever state variable updates,,react triggers Reconcialation cycles(re-renders the componenent)
+
 const fetchdata = async ()=>{
-    const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=30.4765796&lng=76.5905317&carousel=true&third_party_vendor=1");
+    const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
 
     console.log(json);
-    setlistrestraun(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setlistrestraun(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setfilteredrestraun(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 }
 // Conditional rendering :- rendering on the basis of condition is known as conditional rendering
 /*
@@ -95,25 +99,42 @@ if(listrestraun.length == 0){
     //                 "totalRatingsString": "1K+",
     //                 },
     //             }];
-    return listrestraun.length==0?<Shimmer/> : (
+    return  listrestraun.length ==0 ?(<Shimmer/>) : (
     <div className="Body">
         <div className="filter">
             <div className="search">
-            <div className="inputBox_container ">
-                <svg className="search_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" alt="search icon">
-                <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
-                </svg>
-            <input className="inputBox search-box" id="inputBox" type="text" placeholder="Search For Restaurant" value={searchtext} onClick={()=>{
-                
-            }}/>
-            </div>
-            <button className="Search-btn btncss">Search</button>
+                <div className="inputBox_container ">
+                    <svg className="search_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" alt="search icon">
+                    <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
+                    </svg>
+                    <input className="inputBox search-box" 
+                    id="inputBox" type="text" placeholder="Search For Restaurant" 
+                    value={searchtext} onChange={(e)=>{
+                    setsearchtext(e.target.value)
+                    console.log(searchtext)
+                    const filtererestaurant=listrestraun.filter((res)=>
+                        res?.info?.name?.toLowerCase().includes(searchtext.toLowerCase()) 
+                    )
+                    setfilteredrestraun(filtererestaurant);
+                    setsearchtext(e.target.value)
+                } }
+                    />
+                </div>
+                <button className="Search-btn btncss" onClick={
+                    ()=>{
+                        const filtererestaurant=listrestraun.filter((res)=>
+                            res?.info?.name?.toLowerCase().includes(searchtext.toLowerCase()) 
+                        )
+                        setfilteredrestraun(filtererestaurant);
+                    }
+    
+                }>Search</button>
             </div>
             <button className="filter-btn btncss" onClick={()=>{
             //filter logic here
             console.log("fg")
             const filteredlistrestraun=listrestraun.filter((res)=>res?.info?.avgRating>=4);
-            setlistrestraun(filteredlistrestraun);
+            setfilteredrestraun(filteredlistrestraun);
 
 
         }}>Top Rated Restraun..</button>
@@ -121,7 +142,7 @@ if(listrestraun.length == 0){
         <div className="restcontainer" /*style={style} */ >
             {/* <Restaurant resdata={resobj[0]}/>
             <Restaurant resdata={resobj[1]}/> */
-            listrestraun.map((restaurant,index)=>(<Restaurant key={restaurant?.info?.id} resdata={restaurant}/>))
+            filteredrestraun.map((restaurant,index)=>(<Restaurant key={restaurant?.info?.id} resdata={restaurant}/>))
             }
             {/* we use index as key also
 
