eb8e7ec59447afe13b557f5e3c2881acd5895061
rachitsgh <rachit2351@gmail.com>
2024-01-16 15:52:29 +0530

    created aboutus page component

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+const About = ()=>{
+    
+    return(
+        <div>
+            {/* <Header/> */}
+            <h1>About</h1>
+            <h2>This is about us page</h2>
+        </div>
+    )
+}
+
+export default About;
