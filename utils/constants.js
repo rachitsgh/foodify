3f754ae5d7867237ca1901d6833f8c7b1c01b75a
rachitsgh <rachit2351@gmail.com>
2024-02-08 00:25:22 +0530

    add food_item_url

--- utils/constants.js
+++ utils/constants.js
@@ -6,7 +6,9 @@ export const ABOUT_IMG_URL="https://img.freepik.com/premium-photo/burger-white-i
 
 export const MENU_API ="https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId="
 //Above export is known named export 
+export const restcloudinary_URL="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
+export const FOOD_IMG_URL="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
 
 // in one file only one export default can exist
 
